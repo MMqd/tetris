afe3303a9ac0600e1d7e2086dc018680de6c1824
MMqd <85073526+MMqd@users.noreply.github.com>
2022-07-10 16:56:25 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 /*
-	This is a simple tetris game, and an example of doing handling basic terminal input/output on linux, without ncurses.
-	Copyright (C) 2021  MMqd
-	 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+	"tetris" is a simple tetris game, and an example of handling basic terminal input/output on linux, without ncurses.
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2021  MMqd
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	You should have received a copy of the GNU General Public License along
-	with this program; if not, write to the Free Software Foundation, Inc.,
-	51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301 USA.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include  <iostream>
